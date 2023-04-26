0985cc49f1f5bed771e9d91cf126fb79bc1db478
ueunli <cdenen20@gmail.com>
2023-04-26 12:50:03 +0900

    Refactor: DailyBoxOffice의 CodingKey 및 이니셜라이저 추가

--- BoxOffice/Model/MovieDTO/BoxOfficeResult.swift
+++ BoxOffice/Model/MovieDTO/BoxOfficeResult.swift
@@ -5,9 +5,26 @@
 //  Created by 신동오 on 2023/04/26.
 //
 
-struct BoxOfficeResult: Codable {
+struct BoxOfficeResult: Decodable {
     
     let boxofficeType, showRange: String
-    let dailyBoxOfficeList: [Movie]
+    let movies: [Movie]
+    
+    init(from decoder: Decoder) throws {
+        let keys = try decoder.container(keyedBy: ResultKeys.self)
+        let nestedKeys = try keys.nestedContainer(keyedBy: CodingKeys.self, forKey: .boxOfficeResult)
+        self.boxofficeType = try nestedKeys.decode(String.self, forKey: .boxofficeType)
+        self.showRange = try nestedKeys.decode(String.self, forKey: .showRange)
+        self.movies = try nestedKeys.decode([Movie].self, forKey: .movies)
+    }
+    
+    enum ResultKeys: String, CodingKey {
+        case boxOfficeResult
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case showRange, boxofficeType
+        case movies = "dailyBoxOfficeList"
+    }
     
 }
--- BoxOffice/Model/MovieDTO/Movie.swift
+++ BoxOffice/Model/MovieDTO/Movie.swift
@@ -7,20 +7,35 @@
 
 struct Movie: Codable {
     
-    let rank, rankInten, rankOldAndNew, code, name, openDate, salesAmount, salesShare, salesInten, salesChange, salesAcc, audienceCount, audienceInten, audienceChange, audienceAcc, screenCount, showCount: String
-    
+    let rankNumber, rank, rankInten, rankOldAndNew: String
+    let code, name, openDate: String
+    let salesAmount, salesShare, salesInten, salesChange, salesAcc: String
+    let audienceCount, audienceInten, audienceChange, audienceAcc: String
+    let screenCount, showCount: String
+
     enum CodingKeys: String, CodingKey {
-        case code = "movieCD"
+        case rankNumber = "rnum"
+        case rank
+        case rankInten
+        case rankOldAndNew
+        
+        case code = "movieCd"
         case name = "movieNm"
         case openDate = "openDt"
+        
         case salesAmount = "salesAmt"
+        case salesShare = "salesShare"
+        case salesInten
+        case salesChange
+        case salesAcc = "salesAcc"
+        
         case audienceCount = "audiCnt"
         case audienceInten = "audiInten"
         case audienceChange = "audiChange"
         case audienceAcc = "audiAcc"
+        
         case screenCount = "scrnCnt"
         case showCount = "showCnt"
-        case rank, rankInten, rankOldAndNew, salesShare, salesInten, salesChange, salesAcc
     }
     
 }
